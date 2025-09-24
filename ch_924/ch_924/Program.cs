c38ba489670a08e7c4a8f4faa3f6bbfc6981f56d
LNT0420 <d11416102@gs.takming.edu.tw>
2025-09-24 10:41:43 +0800

    v1
    v1

---
+++ ch_924/ch_924/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch_924
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
